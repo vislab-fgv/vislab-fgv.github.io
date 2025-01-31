105f63a9d61b3c49478b69f150170aafa0523a0d
Joel Perca <joelperca.vc@gmail.com>
2025-01-31 18:39:01 -0300

    ci: 🎡 config deploy GP

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://vislab-fgv.pages.github.io/",
   vite: {
     plugins: [tailwindcss()],
   },
