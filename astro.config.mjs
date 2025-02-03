9d848f281ba3acd07d23b2ec11f216a6c919168d
Joel Perca <joelperca.vc@gmail.com>
2025-02-02 21:29:29 -0300

    ci: 🎡 fix base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://vislab-fgv.github.io",
-  base: "/vislab-fgv.github.io",
+  base: "/",
   vite: {
     plugins: [tailwindcss()],
   },
